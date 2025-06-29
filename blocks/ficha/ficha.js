e24400ec2ef08c341dc7a76a1a57cf086deac28f
vidaljuanp <vidaljuanp@gmail.com>
2025-06-29 16:51:43 +0200

    change ficha

--- blocks/ficha/ficha.js
+++ blocks/ficha/ficha.js
@@ -33,23 +33,72 @@ fetch(AEM_HOST + '/graphql/execute.json/aem-demo-assets/item-by-id;slug=' + slug
             const cover_image_path = AEM_HOST + response.data.peliculaList.items[0].cover._path;
             const titulo =  response.data.peliculaList.items[0].titulo;
             const calificacion =  response.data.peliculaList.items[0].calificacion;
-            const sinopsis =  response.data.peliculaList.items[0].sinopsis;
-            const info =  response.data.peliculaList.items[0].info;
-            const equipotecnico =  response.data.peliculaList.items[0].equipotecnico;
+            const sinopsis =  response.data.peliculaList.items[0].sinopsis.plaintext;
+            const info =  response.data.peliculaList.items[0].info.plaintext;
+            const equipotecnico =  response.data.peliculaList.items[0].equipotecnico.plaintext;
 
             fichaDiv.innerHTML = `
-            <section>
+
               <div class="ficha-container">
                 <div class="ficha-column ficha-column-left">
-                    <section><img src="${cover_image_path}"></section>
+
+                    <img src="${cover_image_path}">
+                    Valoracion de usuarios
+
+
+                <div class="ee-stars">
+                
+                <div class="rating-content j-stars">
+                  <div class="stars">
+                    <a href="https://www.movistarplus.es/valoracion?p=5&amp;elemento=519034" title="Muy bien"
+                      class="vote-5 " rel="nofollow" data-telemetry-payload="seccion:rating_5"><span>5</span></a>
+                    <a href="https://www.movistarplus.es/valoracion?p=4&amp;elemento=519034" title="Bien"
+                      class="vote-4 " rel="nofollow" data-telemetry-payload="seccion:rating_4"><span>4</span></a>
+                    <a href="https://www.movistarplus.es/valoracion?p=3&amp;elemento=519034" title="Regular"
+                      class="vote-3  fill " rel="nofollow" data-telemetry-payload="seccion:rating_3"><span>3</span></a>
+                    <a href="https://www.movistarplus.es/valoracion?p=2&amp;elemento=519034" title="Mal"
+                      class="vote-2  fill " rel="nofollow" data-telemetry-payload="seccion:rating_2"><span>2</span></a>
+                    <a href="https://www.movistarplus.es/valoracion?p=1&amp;elemento=519034" title="Muy mal"
+                      class="vote-1  fill " rel="nofollow" data-telemetry-payload="seccion:rating_1"><span>1</span></a>
+                  </div>
+                  <div class="rating-value">
+                    <span class="rating">3</span>
+                    <span class="ratingC">733</span>
+                    <span>votos</span>
+                  </div>
+                </div>
+              </div>
+
+
+
+
+
+
+
                 </div>
                 
                 <div class="ficha-column ficha-column-right">
-                    <h3>Right Column</h3>
-                    <p>This column takes up two-thirds of the width and drops below on smaller screens.</p>
+                    
+                    <h4>Titulo</h4>
+                    <p>${titulo}</p>
+
+                    <h4>Sinopsis</h4>
+                    <p>${sinopsis}</p>
+
+                    <h4>info</h4>
+                    <p>${info}</p>
+
+                    <h4>Calificacion</h4>
+                    <p>${calificacion}</p>
+                    
+                    <h4>Equipo Tecnico</h4>
+                    <p>${equipotecnico}</p>
+
+
+                    
                 </div>
             </div>
-            </section>
+
             `;
 
 
@@ -89,6 +138,8 @@ fetch(AEM_HOST + '/graphql/execute.json/aem-demo-assets/item-by-id;slug=' + slug
 });
 
 }
+
+
 
 
 
